6069f35c5c207d228b59a94a9994794e3d6f89d4
Gabriel Machado <mrsurvival1234@gmail.com>
2023-03-02 13:51:11 -0300

    tomando uma surra do gitignore

--- gambiara.js
+++ gambiara.js
@@ -1,3 +1,6 @@
-let MONGOOSE_CONNECTION_URI = process.env.MONGOOSE_CONNECTION_URI
-let PORT = process.env.PORT
-module.exports = {MONGOOSE_CONNECTION_URI, PORT}
+require('dotenv').config()
+
+const MONGOOSE_CONNECTION_URI = process.env.MONGOOSE_CONNECTION_URI
+const PORT = process.env.PORT
+
+module.exports = {MONGOOSE_CONNECTION_URI: MONGOOSE_CONNECTION_URI, PORT: PORT}
