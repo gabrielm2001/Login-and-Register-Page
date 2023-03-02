1f4acd4433797288329c14a243e8eef63f2a477c
Gabriel Machado <mrsurvival1234@gmail.com>
2023-03-02 15:03:42 -0300

    socorro

--- gambiara.js
+++
@@ -1,6 +0,0 @@
-require('dotenv').config()
-
-const MONGOOSE_CONNECTION_URI = process.env.MONGOOSE_CONNECTION_URI
-const PORT = process.env.PORT
-
-module.exports = {MONGOOSE_CONNECTION_URI: MONGOOSE_CONNECTION_URI, PORT: PORT}
